0accb183a428c4cc7bdef4b7b3380cd17a2e0c42
veron-anna <58977054+veron-anna@users.noreply.github.com>
2019-12-23 14:10:42 +0700

    deploy app

--- App-master/index.js
+++ App-master/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
 app.use(express.json({ extended: false }))
-app.get('/', (req,res) => res.send('API работает'));
 
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/auth/', require('./routes/auth'));
 app.use('/api/posts/', require('./routes/posts'));
+
+if(process.env.NODE_ENV ==='production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен ${PORT}`));
