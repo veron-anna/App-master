a348b0cc73642aaa89f1728fdbc2ed9ab9b1cf83
veron-anna <58977054+veron-anna@users.noreply.github.com>
2019-12-23 14:54:10 +0700

    deploy app

--- App-master/index.js
+++ App-master/index.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV ==='production') {
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+} 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен ${PORT}`));
